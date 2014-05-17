628fdb177f80bb135a3f7e44de7ca4152671d79a
Cyprien Le Pannérer <cyplp@free.fr>
2014-05-17 17:21:26 +0200

    Add send email for new account

--- chaton/utils.py
+++ chaton/utils.py
@@ -1,11 +1,30 @@
+# -*- coding: utf-8 -*-
 import argparse
 import ConfigParser
+import smtplib
+from email.mime.text import MIMEText
 
 import couchdbkit
 import bcrypt
 
 from chaton.models.user import User
 
+template = u"""
+Bonjour %s,
+
+Bienvenu sur %s notre plateforme secrète d'échange de vidéos.
+
+Ton login est «%s».
+Ton mot de passe est «%s».
+
+Tu peux changer ton mot de passe ici : %s/myaccount
+
+Tu peux uploadé tes vidéos (ça prends plombe.) ou voir celle des autres (ça mets aussi une plombe).
+
+
+--
+cyplp@free.fr
+"""
 
 def addUser():
     """
@@ -38,11 +57,26 @@ def addUser():
     admin = args.admin.lower() == 'true'
     password = bcrypt.hashpw(args.password.encode('utf-8'),
                              bcrypt.gensalt())
+
     user = User(password=password,
-                name=args.name,
+                name=args.name.decode('utf-8'),
                 mail=args.userid,
                 isAdmin=admin,
                 )
     user._id = args.userid
     user.save()
+    print template % (args.name.decode('utf-8'), config.get('app:main', 'public_url'),
+                               args.userid.decode('utf-8'), args.password, config.get('app:main', 'public_url'))
+    msg = MIMEText(template % (args.name.decode('utf-8'), config.get('app:main', 'public_url'),
+                               args.userid.decode('utf-8'), args.password, config.get('app:main', 'public_url')), 'plain', 'utf-8')
 
+    msg['Subject'] = 'échange de vidéo pour le mariage de Julien et Isabelle'.decode('utf-8')
+    msg['From'] = 'cyplp@free.fr'
+    msg['To'] = args.userid
+
+    # Send the message via our own SMTP server, but don't include the
+    # envelope header.
+    print msg
+    s = smtplib.SMTP('smtp.free.fr')
+    s.sendmail('cyplp@free.fr', [args.userid], msg.as_string().encode('ascii'))
+    s.quit()
