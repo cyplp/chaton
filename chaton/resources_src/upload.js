cf3d7441bb3842dc4d013135956158eb9e7db0d1
Cyprien Le Pannérer <cyplp@free.fr>
2014-05-31 17:08:45 +0200

    total rewriting

--- chaton/resources_src/upload.js
+++ chaton/resources_src/upload.js
@@ -1,27 +1,42 @@
+
+
 $( document ).ready(function() {
-    $(function () {
-	$('#fileupload').fileupload({
-            dataType: 'json',
-            done: function (e, data) {
-		$.each(data.result.files, function (index, file) {
-                    $('<p/>').text(file.name).appendTo(document.body);
-		});
-            },
-	    progressall: function (e, data) {
-		var progress = parseInt(data.loaded / data.total * 100, 10);
-		$('#progress .bar').css(
-		    'width',
-		    progress + '%'
-		);
+    function progressHandlingFunction(e){
+	console.log(e);
+	if(e.lengthComputable){
+            $('#progress').attr({value:e.loaded,max:e.total});
+	}
+    }
+    $("#goUpload").click(function(){
+	var title = $('#title').val();
+	var description = $('#description').val();
+	var route = $('#formMeta').attr('action');
+	var id = ''
+	var formData = new FormData($('#formMeta')[0]);
+	$.ajax({
+	    url: route,
+	    type: 'POST',
+	    xhr: function() {  // Custom XMLHttpRequest
+		var myXhr = $.ajaxSettings.xhr();
+		if(myXhr.upload){ // Check if upload property exists
+		    myXhr.upload.addEventListener('progress',progressHandlingFunction, false); // For handling the progress of the upload
+		}
+		return myXhr;
 	    },
-	    add: function (e, data) {
-		data.context = $('<button/>').text('Upload')
-                    .appendTo(document.body)
-                    .click(function () {
-			data.context = $('<p/>').text('Uploading...').replaceAll($(this));
-			data.submit();
-                    });
-            }
+	    //Ajax events
+	    //		       beforeSend: beforeSendHandler,
+	    //		       success: completeHandler,
+	    //		       error: errorHandler,
+		       // Form data
+	    data: formData,
+	    //Options to tell jQuery not to process data or worry about content-type.
+	    cache: false,
+	    contentType: false,
+	    processData: false
 	});
-    });
+	console.log("here 2")
+
+    }
+			);
+
 });
